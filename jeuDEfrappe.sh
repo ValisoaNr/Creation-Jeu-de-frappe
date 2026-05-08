e70bb18a9967ea26f782c1e0579538cac1319d85
ValisoaNr <valisoandr12@gmail.com>
2026-05-08 18:33:30 +0300

    Preconfiguration de l'affichage

--- jeuDEfrappe.sh
+++ jeuDEfrappe.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-echo -e "\033[34m"
+rouge="\033[31m"
+bleu="\033[34m"
+vert="\033[32m"
+neutre="\033[0m"
+
+clear
+echo -e "${bleu}"
 cat jeuDEfrappe.ascii
-echo -e "\033[0m"
+echo -e "${neutre}"
+
+echo -e "\tBIENVENUE DANS LE JEU ! \n\n --> apprennez votre en jouant ce jeu ;\n"
+echo -e "${vert}"
+read -sp "Entrez pour continuer ..."
+echo -e "${neutre}"
+echo -e "\n"
